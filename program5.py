00fed3b553a06d2d2f300416b20603ba5eadfe53
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-26 19:57:15 +0530

    Create program5.py

---
+++ program5.py
@@ -0,0 +1,12 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "1234":
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
+
+# Output:
+# Enter username: admin
+# Enter password: 1234
+# Login Successful
